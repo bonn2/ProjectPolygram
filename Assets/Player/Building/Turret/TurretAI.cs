697639d633e1b4466a0f2a9571e2997d0942a24a
Vitzual <65577955+Vitzual@users.noreply.github.com>
2020-10-15 14:49:44 -0700

    Added base turret AI

--- Assets/Player/Building/Turret/TurretAI.cs
+++ Assets/Player/Building/Turret/TurretAI.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class TurretAI : MonoBehaviour
+public class TurretAI : EntityClass
 {
 
-    [SerializeField]
-    private Transform FirePoint;
+    // Turret object information
     private Rigidbody2D turret;
 
+    // Default weapon variables
+    protected float thrust = 1.0f;
+    protected float fireRate = 0.5f;
+    protected float nextFire = -1f;
+    protected float bulletForce = 20f;
+    protected int offset = 0;
+
+    // Base weapon objects
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    public ParticleSystem hitEffect;
+    
     void Start()
     {
         turret = this.GetComponent<Rigidbody2D>();
@@ -25,8 +34,40 @@ public class TurretAI : MonoBehaviour
         turret.rotation = angle;
 
         // Shoot bullet
+        if (nextFire > 0)
+        {
+            nextFire -= Time.deltaTime;
+            return;
+        }
 
+        // Call shoot function, update cooldown
+        Shoot();
+        Cooldown();
+    }
 
+    // Creates bullet object
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        bullet.transform.Rotate(0, 0, offset);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(rotate(firePoint.up, Random.Range(-0.1f, 0.1f)) * bulletForce, ForceMode2D.Impulse);
+        Destroy(bullet, 1f);
+    }
+
+    // Bullet cooldown when holding
+    void Cooldown()
+    {
+        nextFire = fireRate;
+    }
+
+    // Rotation function
+    private static Vector2 rotate(Vector2 v, float delta)
+    {
+        return new Vector2(
+            v.x * Mathf.Cos(delta) - v.y * Mathf.Sin(delta),
+            v.x * Mathf.Sin(delta) + v.y * Mathf.Cos(delta)
+        );
     }
 
 }
